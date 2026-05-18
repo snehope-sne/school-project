713a91bbf953ad6abe3f01217f997a29dd35bfb4
kwanele <kwanelecne@gmail.com>
2026-05-18 23:40:49 +0200

    connect frontend to Render backend

--- backend/check_duplicate.php
+++ backend/check_duplicate.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://https://school-project-psaa.onrender.com");
+header("Access-Control-Allow-Origin: https://school-project-psaa.onrender.com");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
