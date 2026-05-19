05c601fd7fbe8812fac2b21fdbdc079ee45428d4
kwanele <kwanelecne@gmail.com>
2026-05-19 08:18:53 +0200

    fix CORS origin to point to frontend URL

--- backend/login.php
+++ backend/login.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://school-project-psaa.onrender.com");
+header("Access-Control-Allow-Origin: https://school-project-1-xdoe.onrender.com");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
