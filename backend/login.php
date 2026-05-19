9619a48a9f6d069ea98328041e3ec8a0baa99553
kwanele <kwanelecne@gmail.com>
2026-05-19 08:09:20 +0200

    login fixing

--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://school-project-psaa.onrender.com");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
